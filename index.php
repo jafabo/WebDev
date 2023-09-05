d1ae476701a6412823e626f30d4c90f7a5b021b9
Michael Weinberger <minormajorman@gmail.com>
2023-09-05 17:25:58 -0400

    added localStorage

--- index.php
+++ index.php
@@ -4,18 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Color Changer</title>
+    <!-- Import jQuery library -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
+        // Document Ready function ensures code runs after the DOM is fully loaded
         $(document).ready(function(){
+            
+            // Retrieve the saved background color from localStorage if available
+            var savedColor = localStorage.getItem("bgColor");
+            if (savedColor) {
+                $("body").css("background-color", savedColor);
+            }
+            
+            // Listen for a button click to change the background color
             $("#changeColorButton").click(function(){
+                
+                // Perform a GET request to the PHP file, which will return a new color
                 $.get("color.php", function(data) {
+                    
+                    // Change the background color to the new color
                     $("body").css("background-color", data);
+                    
+                    // Save the new background color in localStorage
+                    localStorage.setItem("bgColor", data);
                 });
             });
         });
     </script>
 </head>
 <body>
-    <button id="changeColorButton">Change Background Color!</button>
+    <!-- Button that will trigger the background color change -->
+    <button id="changeColorButton">Change Background Color</button>
 </body>
 </html>
