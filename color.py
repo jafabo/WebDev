2a0c90bbfb6d502d6ae191e5e10cef3da4a0a6d5
Michael Weinberger <minormajorman@gmail.com>
2023-09-05 16:46:57 -0400

    this is just a test

---
+++ color.py
@@ -0,0 +1,9 @@
+import random
+
+def generate_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return f"rgb({r},{g},{b})"
+
+print(generate_random_color())
