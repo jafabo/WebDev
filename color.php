efa8c309d401bfbf6b55412d85604145d727eae0
Michael Weinberger <minormajorman@gmail.com>
2023-09-05 17:27:01 -0400

    weeee

--- color.php
+++ color.php
@@ -1,4 +1,7 @@
 <?php
+    // Execute the Python script and save the output in a variable
     $output = shell_exec('python3 color.py');
+    
+    // Echo the output (new color) back to the client
     echo $output;
 ?>
--- color.py
+++ color.py
@@ -1,9 +1,14 @@
 import random
 
+# Function to generate a random color
 def generate_random_color():
+    # Generate random values for Red, Green, and Blue
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
+    
+    # Return the color in RGB format
     return f"rgb({r},{g},{b})"
 
+# Print the generated color so that the PHP script can capture this output
 print(generate_random_color())
