79f25450d56ce352de7ebbaf9a438a73d2e7ba5f
Michael Weinberger <minormajorman@gmail.com>
2023-09-18 12:20:47 -0400

    changes

--- web-files/index.php
+++ web-files/index.php
@@ -10,7 +10,7 @@
     <nav class="navbar">
         <a href="index.php">Home</a>
         <a href="color-changer.php">Color Changer</a>
-        <a href="#">Kubernetes Overview</a>
+        <a href="k8s-overview.php">Kubernetes Overview</a>
         <a href="#">K8s Evolution</a>
         <a href="#">Web Apps & K8s</a>
         <a href="#">K8s Benefits</a>
