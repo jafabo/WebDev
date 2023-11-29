6651ea0f0787f8b954ed5b97f07591f4a49c2059
Michael Weinberger <minormajorman@gmail.com>
2023-11-29 12:09:35 -0500

    things

--- web-files/vertNav.js
+++ web-files/vertNav.js
@@ -4,9 +4,17 @@ function showVerticalNav() {
     var topNavbarHeight = document.querySelector('.navbar').offsetHeight;
     var verticalNavbar = document.getElementById("verticalNavbar");
 
-    if (window.pageYOffset >= topNavbarHeight) {
-        verticalNavbar.style.display = "block";
+    // Check if the page is scrolled beyond the height of the top navbar
+    if (window.pageYOffset > topNavbarHeight) {
+        verticalNavbar.style.opacity = "1";
+        verticalNavbar.style.visibility = "visible";
     } else {
-        verticalNavbar.style.display = "none";
+        // Use a transition end event listener to hide the navbar after the transition
+        verticalNavbar.style.opacity = "0";
+        verticalNavbar.addEventListener('transitionend', function() {
+            if (window.pageYOffset <= topNavbarHeight) {
+                verticalNavbar.style.visibility = "hidden";
+            }
+        }, { once: true }); // Event listener will trigger only once
     }
 }
